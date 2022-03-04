7830add38cc6e195cfcfc6e6a76bc719e0146a7d
enevas <enevas@vmware.com>
2022-03-04 13:23:05 +0200

    [UI] Updated dashboard page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -25,27 +25,12 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Swappers</title>
+        <title>Swappers | Dashboard</title>
         <meta name="description" content="Swap, sell, and buy collecyable items" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Navbar />
-      <div className="container mx-auto max-w-5xl my-20">
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {data?.allUsers.map(user => (
-            <li key={user.id} className="shadow  max-w-md  rounded">
-              <div className="p-5 flex flex-col space-y-2">
-                <p className="text-sm text-blue-500">{user.firstName}</p>
-                <p className="text-lg font-medium">{user.lastName}</p>
-                <p className="text-gray-600">{user.email}</p>
-                <p className="text-gray-600">{user.role}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-
       <UsersTable></UsersTable>
     </div>
   )
