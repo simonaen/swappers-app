ed9cff096fa0e4cf1f8dcc50de6fc03e5c2ca6fd
enevas <enevas@vmware.com>
2022-03-04 14:50:33 +0200

    [UI] Added dashboard sidebar component

---
+++ components/sidebar/sidebar.component.tsx
@@ -0,0 +1,104 @@
+import { gql, useQuery } from "@apollo/client";
+import { useUser } from "@auth0/nextjs-auth0";
+import { Role } from "@prisma/client";
+import React from "react";
+
+const UserRole = gql`
+  query User($email: String!) {
+    user(email: $email) {
+        id
+        role
+  }
+  }
+`
+
+const Sidebar = ({children}) => {
+    const { data, loading, error } = useQuery(UserRole, {variables: {userEmail: useUser().user?.email}});
+
+    console.log(data)
+
+    return (
+        <div className="flex flex-no-wrap">
+            <div className="w-56 sm:relative shadow md:h-screen flex-col justify-between flex">
+                <div className="px-8">
+                    <ul className="mt-12">
+                        <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
+                            <div className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-grid" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                    <rect x={4} y={4} width={6} height={6} rx={1} />
+                                    <rect x={14} y={4} width={6} height={6} rx={1} />
+                                    <rect x={4} y={14} width={6} height={6} rx={1} />
+                                    <rect x={14} y={14} width={6} height={6} rx={1} />
+                                </svg>
+                                <span className="text-sm  ml-2">Dashboard</span>
+                            </div>
+                        </li>
+                        <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
+                            <div className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-inbox" width={18} height={18} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <rect x="4" y="4" width="16" height="16" rx="2"></rect>
+                                    <path d="M4 13h3l3 3h4l3 -3h3"></path>
+                                </svg>
+                                <span className="text-sm  ml-2">Inbox</span>
+                            </div>
+                        </li>
+                        <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
+                            <div className="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" width={18} height={18} viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+                            </svg>
+                                <span className="text-sm  ml-2">Listings</span>
+                            </div>
+                        </li>
+                        <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
+                            <div className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-code" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                    <polyline points="7 8 3 12 7 16" />
+                                    <polyline points="17 8 21 12 17 16" />
+                                    <line x1={14} y1={4} x2={10} y2={20} />
+                                </svg>
+                                <span className="text-sm  ml-2">Trade history</span>
+                            </div>
+                        </li>
+                        {/* If user is admin display additional links */}
+                        
+                            <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
+                                <div className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" width={18} height={18} viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                                </svg>
+                                    <span className="text-sm  ml-2">Manage Users</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
+                                <div className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" width={18} height={18} viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+                                </svg>
+                                    <span className="text-sm  ml-2">Manage Listings</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6">
+                                <div className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" width={18} height={18} viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
+                                </svg>
+                                    <span className="text-sm  ml-2">Add categories</span>
+                                </div>
+                            </li>
+                        
+                    </ul>
+                </div>
+            </div>
+            
+            <div className="container mx-auto py-10 md:w-4/5 w-11/12 px-6">
+                <div className="w-full h-full">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
