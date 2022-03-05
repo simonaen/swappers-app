a0f7d180698c805549ca24eebf1f050b872f8c5d
enevas <enevas@vmware.com>
2022-03-05 19:00:53 +0200

    [UI] Added listing card component

---
+++ components/listing/listing-card.component.tsx
@@ -0,0 +1,28 @@
+export default function ListingCard(props) {
+
+    return (        
+        <div key={props.listing.id} className="group relative">
+            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+            <img
+                src={props.listing?.contentLinks[0]?.contentLink}
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+            />
+            </div>
+            <div className="mt-4 flex justify-between">
+            <div>
+                <h3 className="text-sm text-gray-700">
+                <a href={'/listing/'+ props.listing.id}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {props.listing.name}
+                </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500 h-4 overflow-y-hidden">{props.listing.description}</p>
+            </div>
+            <p className="text-sm font-medium text-gray-900">${props.listing.price}</p>
+            </div>
+        </div>
+        
+    )
+}
+
+
