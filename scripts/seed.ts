ac8863b51aa4c5918164b7ff354743e506b8e1fe
enevas <enevas@vmware.com>
2022-03-05 18:40:16 +0200

    [UTIL] Updated seed to add contentLinks to item

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -79,6 +79,19 @@ const item = await prisma.item.create({
   }
 })
 console.log(`Created item: ${item.name}`)
+
+await prisma.itemContentLink.createMany({
+  data: [
+    {
+      itemId: item.id,
+      contentLink: 'https://chairish-prod.freetls.fastly.net/image/product/sized/092dde85-404a-4059-aca2-f57634fabeca/moving-must-sell-late-19th-century-english-victorian-mahogany-ball-and-claw-foot-dining-table-8479?aspect=fit&width=1600&height=1600'
+    },
+    {
+      itemId: item.id,
+      contentLink: 'https://chairish-prod.freetls.fastly.net/image/product/sized/3ce82a48-b70e-406e-83a5-7074d04ee1c9/moving-must-sell-late-19th-century-english-victorian-mahogany-ball-and-claw-foot-dining-table-7939?aspect=fit&width=1600&height=1600'
+    }
+]
+})
 }
 
 main()
