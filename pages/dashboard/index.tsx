02dbb574f25ee8006dc10ed0d6316b46446232f5
enevas <enevas@vmware.com>
2022-03-06 12:29:17 +0200

    [UI] Updated dashboard index page

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Navbar from '../../components/navbar/nabvar.component'
+import Sidebar from '../../components/sidebar/sidebar.component'
+import { useUser } from '@auth0/nextjs-auth0'
+
+
+export default function Dashboard() {
+  const { user } = useUser();
+  return (
+    <div>
+      <Head>
+        <title>Swappers | Dashboard</title>
+        <meta name="description" content="Swap, sell, and buy collecyable items" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+      <Sidebar>
+      <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Hi, {user?.name}!</h2>
+      </div>
+      </Sidebar>
+      
+    </div>
+  )
+}
