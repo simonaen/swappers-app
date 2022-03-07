8239cdd209bf7ad056380b45a6a305aa50b29bd9
enevas <enevas@vmware.com>
2022-03-07 13:59:54 +0200

    [UI] Updated search page

--- pages/search/[filter].tsx
+++ pages/search/[filter].tsx
@@ -7,24 +7,34 @@ import Navbar from "../../components/navbar/nabvar.component";
 import Searchbar from "../../components/searchbar/searchbar.component";
 
 const FilterItems = gql`
-  query filterItems($filter: String) {
-    filterItems(filter: $filter) {
-        id
-        name
-        description
-        price
-        sold
-        contentLinks {
-            contentLink
+  query filterItemsPaginated($filter: String!, $first: Int, $after: String) {
+    filterItemsPaginated(filter: $filter, first: $first, after: $after) {
+        pageInfo {
+            endCursor
+            hasNextPage
         }
-  }
+        edges {
+            cursor
+            node {
+                id
+                name
+                description
+                price
+                sold
+                contentLinks {
+                    contentLink
+                }
+            }
+        }
+    }
   }
 `
 
 export default function FilterResults () {
     const router = useRouter();
     const { filter } = router.query;
-    const { data, loading, error } = useQuery(FilterItems, {variables: {filter: filter}});
+    const { data, loading, error, fetchMore } = useQuery(FilterItems, {variables: {filter: filter, first: 7}});
+
 
     return (
         <div>
@@ -38,18 +48,45 @@ export default function FilterResults () {
         <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 py-8">
             <h2 className="text-2xl mb-4 font-extrabold tracking-tight text-gray-900">Search results for: {filter}</h2>
             <Searchbar/>
-            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            <div className="max-h-screen overflow-scroll">
+            <div className="mt-6 grid grid-cols-1 gap-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             { loading ? (
                 <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
               </svg>
             ) : (
-               data?.filterItems.length ? ( data?.filterItems.map((listing) => (
-                <ListingCard key={listing.id} listing={listing}/>
+               data?.filterItemsPaginated.edges.length ? ( data?.filterItemsPaginated.edges.map((listing) => (
+                <ListingCard key={listing.node.id} listing={listing.node}/>
                 ))) : (
                 <div className="text-xl font-extrabold tracking-tight text-gray-500">No results found :(</div>
                 )
             )}
-            
+            <div className="flex flex-col-reverse">
+            {data?.filterItemsPaginated?.pageInfo?.hasNextPage ? (
+                
+                <p
+                    className="cursor-pointer text-gray-500 hover:text-blue-400 font-medium text-center"
+                    onClick={() => {
+                    fetchMore({
+                        variables: { after: data?.filterItemsPaginated?.pageInfo?.endCursor },
+                        updateQuery: (prevResult, { fetchMoreResult }) => {
+                        fetchMoreResult.links?.edges = [
+                            ...prevResult.links?.edges,
+                            ...fetchMoreResult.links?.edges,
+                        ];
+                        return fetchMoreResult;
+                        },
+                    });
+                    }}
+                >
+                    load more
+                </p>
+                ) : (
+                <p className="text-center font-medium">
+                    You've reached the end!{" "}
+                </p>
+            )}
+            </div>
+            </div>
             </div>
         </div>
         </div>
